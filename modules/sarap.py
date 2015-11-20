a1f224c869fbc835424993aa6779dd2f45e70c61
Onur Aslan <onuraslan@gmail.com>
2015-11-20 17:47:19 +0200

    Add sarap module

---
+++ modules/sarap.py
@@ -0,0 +1,17 @@
+
+from sopel import module
+import time
+
+
+@module.commands('sarap')
+@module.commands('s')
+@module.thread(False)
+def sarap(bot, trigger):
+    # sarabi koydugumuz tarih
+    sarap = time.mktime(time.strptime('20 Nov 2015', '%d %b %Y'))
+    delta = time.time() - sarap
+
+    # deltayi gune cevirelim
+    gun = int(round(delta / (60 * 60 * 24)))
+
+    bot.reply('Sarab gun sayisi: ' + gun)
